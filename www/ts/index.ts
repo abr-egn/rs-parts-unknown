18f986fef75234df083ef6550a836b7a6b24478c
Abraham Egnor <abe.egnor@gmail.com>
2020-03-24 20:16:18 -0400

    wire up ts-interface-checker

--- www/ts/index.ts
+++ www/ts/index.ts
@@ -1,5 +1,13 @@
-//import {createCheckers} from "ts-interface-checker";
+import {createCheckers} from "ts-interface-checker";
+import dataTI from "./data-ti";
+
+const {Display} = createCheckers(dataTI);
 
 import('../wasm').then(rust => {
   console.log(rust.add(1, 2));
+
+  let game = new rust.PartsUnknown();
+  let display = game.get_display();
+  Display.check(display);
+  console.log(display);
 }).catch(console.error);
