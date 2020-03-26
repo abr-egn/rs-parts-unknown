befbc8b2abf8b970f936313a24200daf46518213
Abraham Egnor <abe.egnor@gmail.com>
2020-03-26 16:17:22 -0400

    ts: hex <-> pixel

--- www/ts/render.ts
+++ www/ts/render.ts
@@ -1,3 +1,7 @@
+import {Hex} from "./data";
+
+const HEX_SIZE = 30;
+
 export class Engine {
     private readonly ctx: CanvasRenderingContext2D;
     constructor(private readonly canvas: HTMLCanvasElement) {
@@ -11,12 +15,11 @@ export class Engine {
         this.ctx.translate(this.canvas.width / 2, this.canvas.height / 2);
 
         const DELTA = Math.PI/3.0;
-        const SIZE = 30;
         this.ctx.beginPath();
-        this.ctx.moveTo(Math.cos(0)*SIZE, Math.sin(0)*SIZE);
+        this.ctx.moveTo(Math.cos(0)*HEX_SIZE, Math.sin(0)*HEX_SIZE);
         for (let i = 1; i < 6; i++) {
-            let x = Math.cos(i*DELTA)*SIZE;
-            let y = Math.sin(i*DELTA)*SIZE;
+            let x = Math.cos(i*DELTA)*HEX_SIZE;
+            let y = Math.sin(i*DELTA)*HEX_SIZE;
             this.ctx.lineTo(x, y);
         }
         this.ctx.closePath();
@@ -31,7 +34,9 @@ export class Engine {
     onMouseDown(event: MouseEvent) {
         console.log("click!");
         console.log(event);
-        console.log(this.mouseCoords(event))
+        const point = this.mouseCoords(event);
+        console.log(point);
+        console.log(pixelToHex(point));
     }
 
     mouseCoords(event: MouseEvent): DOMPointReadOnly {
@@ -39,5 +44,38 @@ export class Engine {
         const screenPoint = new DOMPointReadOnly(
             event.clientX - rect.left, event.clientY - rect.top);
         return screenPoint.matrixTransform(this.ctx.getTransform().inverse());
+    }   
+}
+
+function hexToPixel(hex: Hex): DOMPointReadOnly {
+    const x = HEX_SIZE * 3/2 * hex.x;
+    const y = HEX_SIZE * (Math.sqrt(3)/2 * hex.x + Math.sqrt(3) * hex.y);
+    return new DOMPointReadOnly(x, y);
+}
+
+function pixelToHex(point: DOMPointReadOnly): Hex {
+    const x = (2./3 * point.x) / HEX_SIZE;
+    const y = (-1./3 * point.x + Math.sqrt(3)/3 * point.y) / HEX_SIZE;
+    return hexRound({x: x, y: y});
+}
+
+function hexRound(hex: Hex): Hex {
+    const z = -hex.x - hex.y;
+    var rx = Math.round(hex.x);
+    var ry = Math.round(hex.y);
+    var rz = Math.round(z);
+
+    const x_diff = Math.abs(rx - hex.x);
+    const y_diff = Math.abs(ry - hex.y);
+    const z_diff = Math.abs(rz - z);
+
+    if (x_diff > y_diff && x_diff > z_diff) {
+        rx = -ry-rz;
+    } else if (y_diff > z_diff) {
+        ry = -rx-rz;
+    } else {
+        rz = -rx-ry;
     }
+
+    return {x: rx, y: ry};
 }
