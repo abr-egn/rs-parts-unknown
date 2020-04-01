cb3b26588a906d96bda4bcf6f7a1e36d1cc83415
Abraham Egnor <abe.egnor@gmail.com>
2020-04-01 19:15:27 -0400

    rs: work towards meaningful ap cost

--- src/creature.rs
+++ src/creature.rs
@@ -1,21 +1,44 @@
 use wasm_bindgen::prelude::*;
 
 use crate::card::Card;
+use crate::id_map::IdMap;
 
 #[wasm_bindgen]
 #[derive(Debug, Clone)]
 pub struct Creature {
     kind: Kind,
+    parts: IdMap<Part>,
+    cur_ap: i32,
 }
 
 impl Creature {
     pub fn new(kind: Kind) -> Self {
-        Creature { kind }
+        Creature { kind, parts: IdMap::new(), cur_ap: 0 }
     }
 
     pub fn kind(&self) -> &Kind { &self.kind }
-}
 
+    pub fn cards(&self) -> impl Iterator<Item=&Card> {
+        self.parts.map().values()
+            .flat_map(|part| part.cards())
+    }
+
+    pub fn cur_ap(&self) -> i32 { self.cur_ap }
+
+    pub fn max_ap(&self) -> i32 {
+        self.parts.map().values().fold(0, |sum, part| sum + part.ap())
+    }
+
+    pub fn fill_ap(&mut self) {
+        self.cur_ap = self.max_ap();
+    }
+
+    pub fn spend_ap(&mut self, ap: i32) -> bool {
+        if ap > self.cur_ap { return false; }
+        self.cur_ap -= ap;
+        true
+    }
+}
 
 #[derive(Debug, Clone)]
 pub enum Kind {
@@ -45,6 +68,43 @@ pub struct NPC {
     #[wasm_bindgen(skip)]
     pub attack_range: i32,
 }
+
+#[wasm_bindgen]
+#[derive(Debug, Clone)]
+pub struct Part {
+    cards: Vec<Card>,
+    ap: i32,
+    /*
+    power: i32,
+    max_hp: i32,
+    cur_hp: i32,
+    capacity: i32,
+    tags: HashSet<PartTag>,
+    joints: Vec<Joint>,
+    */
+}
+
+impl Part {
+    pub fn new(cards: Vec<Card>, ap: i32) -> Self {
+        Part { cards, ap }
+    }
+    pub fn cards(&self) -> &[Card] { &self.cards }
+    pub fn ap(&self) -> i32 { self.ap }
+}
+
+/*
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
+pub enum PartTag {
+    Head, Torso, Limb, Arm, Leg,
+    Flesh, Metal, Eldritch,
+}
+
+#[derive(Debug, Clone)]
+pub struct Joint {
+    required: HashSet<PartTag>,
+    attached: Option<Id<Part>>,
+}
+*/
 
 mod wasm {
     use js_sys::Array;
