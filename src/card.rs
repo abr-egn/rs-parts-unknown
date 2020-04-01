84d63bec53927f631dcc6971535a9ea296f80ff2
Abraham Egnor <abe.egnor@gmail.com>
2020-03-31 20:42:35 -0400

    rs: work towards cards

--- src/card.rs
+++ src/card.rs
@@ -1,28 +1,33 @@
 #![allow(unused)]
 
+use hex::Hex;
 use wasm_bindgen::prelude::*;
+
+use crate::creature::Creature;
+use crate::id_map::Id;
+use crate::world::World;
 
 #[wasm_bindgen]
 #[derive(Clone)]
 pub struct Card {
     name: String,
-    behavior: Box<dyn Behavior>,
+    ap_cost: i32,
+    start_play: fn(&World, &Id<Creature>) -> Box<dyn Behavior>,
 }
 
 impl Card {
     pub fn name(&self) -> &str { &self.name }
-    pub fn behavior(&self) -> &dyn Behavior { &*self.behavior }
+    pub fn start_play(&self, world: &World, source: &Id<Creature>) -> Box<dyn Behavior> {
+        (self.start_play)(world, source)
+    }
 }
 
-#[wasm_bindgen]
-impl Card {
-    #[wasm_bindgen(js_name = clone)]
-    pub fn js_clone(&self) -> Card { self.clone() }
-    #[wasm_bindgen(getter = name)]
-    pub fn js_name(&self) -> String { self.name.clone() }
-}
-
+// TODO: power scaling
 pub trait Behavior: BehaviorClone {
+    fn highlight(&self, world: &World, cursor: Hex) -> Vec<Hex>;
+    fn lowlight(&self, world: &World, cursor: Hex) -> Vec<Hex>;
+    fn target_valid(&self, world: &World, cursor: Hex) -> bool;
+    fn apply(&self, world: &mut World, target: Hex);
 }
 
 pub trait BehaviorClone {
@@ -37,4 +42,52 @@ impl<T: 'static + Behavior + Clone> BehaviorClone for T {
 
 impl Clone for Box<dyn Behavior> {
     fn clone(&self) -> Self { self.clone_box() }
+}
+
+#[derive(Debug, Clone)]
+pub struct Walk {
+    range: i32,
+    creature_id: Id<Creature>,
+    start: Hex,
+}
+
+impl Behavior for Walk {
+    fn highlight(&self, world: &World, cursor: Hex) -> Vec<Hex> {
+        unimplemented!()
+    }
+    fn lowlight(&self, world: &World, cursor: Hex) -> Vec<Hex> {
+        unimplemented!()
+    }
+    fn target_valid(&self, world: &World, cursor: Hex) -> bool {
+        unimplemented!()
+    }
+    fn apply(&self, world: &mut World, target: Hex) {
+        unimplemented!()
+    }
+}
+
+impl Walk {
+    pub fn behavior(world: &World, source: &Id<Creature>, range: i32) -> Box<dyn Behavior> {
+        let start = world.map().creatures().get(source).unwrap().clone();
+        Box::new(Walk { range, creature_id: *source, start })
+    }
+    pub fn card() -> Card {
+        Card {
+            name: "Walk".into(),
+            ap_cost: 1,
+            start_play: |world, source| Walk::behavior(world, source, 5),
+        }
+    }
+}
+
+mod wasm {
+    use super::*;
+
+    #[wasm_bindgen]
+    impl Card {
+        #[wasm_bindgen(js_name = clone)]
+        pub fn js_clone(&self) -> Card { self.clone() }
+        #[wasm_bindgen(getter = name)]
+        pub fn js_name(&self) -> String { self.name.clone() }
+    }
 }
