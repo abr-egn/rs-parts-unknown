b5fefb9da3de62ebd414965e7a0292f8a9c5909c
Abraham Egnor <abe.egnor@gmail.com>
2020-04-04 14:04:37 -0400

    update RefMut to new scheme

--- src/cell.rs
+++ src/cell.rs
@@ -40,9 +40,11 @@ impl<T> RcCell<T> {
             ptr: self.rc.value.get(),
         }
     }
-    pub fn borrow_mut(&self) -> RefMut<T> { self.borrow_mut_as(|t| t) }
-    pub fn borrow_mut_as<U, F: FnOnce(&mut T) -> &mut U>(&self, f: F) -> RefMut<T, U> {
-        RefMut::new(&self, f).unwrap()
+    pub fn borrow_mut(&self) -> RefMut<T> {
+        RefMut {
+            track: RefMutTrack::new(&self).unwrap(),
+            ptr: self.rc.value.get(),
+        }
     }
     pub fn rc_count(&self) -> usize { Rc::strong_count(&self.rc) }
     pub fn borrow_count(&self) -> Option<usize> {
@@ -50,6 +52,30 @@ impl<T> RcCell<T> {
             Borrow::Ref { count } => Some(count),
             Borrow::RefMut => None,
         }
+    }
+}
+
+pub struct Ref<T, U=T> {
+    track: RefTrack<T>,
+    ptr: *const U,
+}
+
+impl<T, U> Ref<T, U> {
+    pub fn map<V, F>(this: Ref<T, U>, f: F) -> Ref<T, V>
+        where F: FnOnce(&U) -> &V,
+    {
+        Ref {
+            track: this.track,
+            ptr: f(unsafe { &*this.ptr }),
+        }
+    }
+    pub fn map_opt<V, F>(this: Ref<T, U>, f: F) -> Option<Ref<T, V>>
+        where F: FnOnce(&U) -> Option<&V>,
+    {
+        f(unsafe { &*this.ptr }).map(|u| Ref {
+            track: this.track,
+            ptr: u,
+        })
     }
 }
 
@@ -82,66 +108,32 @@ impl<T> Drop for RefTrack<T> {
     }
 }
 
-pub struct Ref<T, U=T> {
-    track: RefTrack<T>,
-    ptr: *const U,
-}
-
-impl<T, U> Ref<T, U> {
-    pub fn map<V, F>(this: Ref<T, U>, f: F) -> Ref<T, V>
-        where F: FnOnce(&U) -> &V,
-    {
-        Ref {
-            track: this.track,
-            ptr: f(unsafe { &*this.ptr }),
-        }
-    }
-    pub fn map_opt<V, F>(this: Ref<T, U>, f: F) -> Option<Ref<T, V>>
-        where F: FnOnce(&U) -> Option<&V>,
-    {
-        f(unsafe { &*this.ptr }).map(|u| Ref {
-            track: this.track,
-            ptr: u,
-        })
-    }
-}
-
-
 impl<T, U> Deref for Ref<T, U> {
     type Target = U;
     fn deref(&self) -> &U { unsafe { &*self.ptr } }
 }
 
 pub struct RefMut<T, U=T> {
-    rc: Rc<Inner<T>>,
+    track: RefMutTrack<T>,
     ptr: *mut U,
 }
 
 impl<T, U> RefMut<T, U> {
-    fn new<F: FnOnce(&mut T) -> &mut U>(cell: &RcCell<T>, f: F) -> Option<RefMut<T, U>> {
-        let b = cell.rc.borrow.get();
-        match b {
-            Borrow::Ref { count: 0 } => {
-                cell.rc.borrow.set(Borrow::RefMut);
-                Some(RefMut {
-                    rc: Rc::clone(&cell.rc),
-                    ptr: f(unsafe { &mut *cell.rc.value.get() }),
-                })
-            }
-            _ => None,
+    pub fn map<V, F>(this: RefMut<T, U>, f: F) -> RefMut<T, V>
+        where F: FnOnce(&mut U) -> &mut V,
+    {
+        RefMut {
+            track: this.track,
+            ptr: f(unsafe { &mut *this.ptr }),
         }
     }
-}
-
-impl<T, U> Drop for RefMut<T, U> {
-    fn drop(&mut self) {
-        let b = self.rc.borrow.get();
-        match b {
-            Borrow::RefMut => {
-                self.rc.borrow.set(Borrow::Ref { count: 0 });
-            }
-            _ => panic!("Invalid borrow on RefMut drop: {:?}", b),
-        }
+    pub fn map_opt<V, F>(this: RefMut<T, U>, f: F) -> Option<RefMut<T, V>>
+        where F: FnOnce(&mut U) -> Option<&mut V>,
+    {
+        f(unsafe { &mut *this.ptr }).map(|u| RefMut {
+            track: this.track,
+            ptr: u,
+        })
     }
 }
 
@@ -152,4 +144,33 @@ impl<T, U> Deref for RefMut<T, U> {
 
 impl<T, U> DerefMut for RefMut<T, U> {
     fn deref_mut(&mut self) -> &mut U { unsafe { &mut *self.ptr } }
+}
+
+struct RefMutTrack<T> {
+    rc: Rc<Inner<T>>
+}
+
+impl<T> RefMutTrack<T> {
+    fn new(cell: &RcCell<T>) -> Option<RefMutTrack<T>> {
+        let b = cell.rc.borrow.get();
+        match b {
+            Borrow::Ref { count: 0 } => {
+                cell.rc.borrow.set(Borrow::RefMut);
+                Some(RefMutTrack { rc: Rc::clone(&cell.rc) })
+            }
+            _ => None,
+        }
+    }
+}
+
+impl<T> Drop for RefMutTrack<T> {
+    fn drop(&mut self) {
+        let b = self.rc.borrow.get();
+        match b {
+            Borrow::RefMut => {
+                self.rc.borrow.set(Borrow::Ref { count: 0 });
+            }
+            _ => panic!("Invalid borrow on RefMut drop: {:?}", b),
+        }
+    }
 }
