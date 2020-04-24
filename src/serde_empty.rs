6b748354674aa7e85d18950e8e166d7e1075501f
Abraham Egnor <abe.egnor@gmail.com>
2020-04-24 14:04:46 -0400

    slightly better empty hack

--- src/serde_empty.rs
+++ src/serde_empty.rs
@@ -1,9 +1,10 @@
 use serde::{
     Deserializer, Serializer,
     de::IgnoredAny,
+    ser::SerializeStruct,
 };
 pub fn serialize<S: Serializer>(s: S) -> Result<S::Ok, S::Error> {
-    s.serialize_bool(true)
+    s.serialize_struct("UNUSED_NAME", 0)?.end()
 }
 pub fn deserialize<'de, D: Deserializer<'de>>(d: D) -> Result<(), D::Error> {
     d.deserialize_any(IgnoredAny).map(|_| ())
