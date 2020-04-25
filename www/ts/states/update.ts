2898e67d808356eb62919cf2b3ce68c2932a2e3d
Abraham Egnor <abe.egnor@gmail.com>
2020-04-25 12:07:49 -0400

    cleanup

--- www/ts/states/update.ts
+++ www/ts/states/update.ts
@@ -12,15 +12,12 @@ export class UpdateState extends State {
     ) { super(); }
 
     async onPushed() {
-        const preview = (ev: wasm.Event) => {
-            this.update((draft) => {
-                draft.build(Preview).addEvent(ev, true);
-            });
-        };
         this.update((draft) => {
             draft.set(Preview);
         });
-        await this._animateEvents(this._events, preview);
+        for (let event of this._events) {
+            await this._animateEvent(event);
+        }
         await window.game.updateWorld(this._nextWorld);
         let state: wasm.GameState;
         switch (state = window.game.world.state()) {
@@ -34,28 +31,28 @@ export class UpdateState extends State {
         }
     }
 
-    private async _animateEvents(events: wasm.Event[], preview: (ev: wasm.Event) => void) {
+    private async _animateEvent(event: wasm.Event) {
         const board = window.game.board;
-        for (let event of events) {
-            preview(event);
-            let data;
-            if (data = event.CreatureMoved) {
-                await board.moveCreatureTo(data.id, hexToPixel(data.to))
-            } else if (data = event.OnCreature) {
-                let ce;
-                if (ce = data.event.OnPart) {
-                    let pe;
-                    if (pe = ce.event.ChangeHP) {
-                        const float = board.hpFloat(data.id, ce.id, pe.delta);
-                        float.style!.animationName = "floatLift";
-                        this.update((draft) => {
-                            draft.build(UpdateState.UI).float.add(float);
-                        });
-                        await new Promise(f => setTimeout(f, 2000));
-                        this.update((draft) => {
-                            draft.build(UpdateState.UI).float.delete(float);
-                        });
-                    }
+        this.update((draft) => {
+            draft.build(Preview).addEvent(event, true);
+        });
+        let data;
+        if (data = event.CreatureMoved) {
+            await board.moveCreatureTo(data.id, hexToPixel(data.to))
+        } else if (data = event.OnCreature) {
+            let ce;
+            if (ce = data.event.OnPart) {
+                let pe;
+                if (pe = ce.event.ChangeHP) {
+                    const float = board.hpFloat(data.id, ce.id, pe.delta);
+                    float.style!.animationName = "floatLift";
+                    this.update((draft) => {
+                        draft.build(UpdateState.UI).float.add(float);
+                    });
+                    await new Promise(f => setTimeout(f, 2000));
+                    this.update((draft) => {
+                        draft.build(UpdateState.UI).float.delete(float);
+                    });
                 }
             }
         }
